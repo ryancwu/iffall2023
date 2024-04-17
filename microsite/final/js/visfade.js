6e596aeb2e084a797f66ca73c18a954c7e882858
Ryan Wu <ryancwu03@gmail.com>
2024-04-17 08:46:30 -0500

    push final

---
+++ microsite/final/js/visfade.js
@@ -0,0 +1,28 @@
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle scroll event
+function handleScroll() {
+  var visfadeItems = document.querySelectorAll(".visfade");
+  visfadeItems.forEach(function (item) {
+    if (isElementInViewport(item)) {
+      item.classList.add("visible");
+    } else {
+      if (!item.classList.contains("perm")) {
+        item.classList.remove("visible");
+      }
+    }
+  });
+}
+
+window.addEventListener("scroll", handleScroll);
+
+handleScroll();
